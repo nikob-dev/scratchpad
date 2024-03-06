a3404ac8dd844efbbf4b571f8947407604a684d7
Nikolas <nikolaslbussey@gmail.com>
2024-03-05 21:09:44 -0500

    new rust project fstar

---
+++ fstar/target/debug/deps/fstar-02ac8a648128a1c2.d
@@ -0,0 +1,5 @@
+/Users/domovoi/Desktop/Projects/scratchpad/fstar/target/debug/deps/libfstar-02ac8a648128a1c2.rmeta: src/main.rs
+
+/Users/domovoi/Desktop/Projects/scratchpad/fstar/target/debug/deps/fstar-02ac8a648128a1c2.d: src/main.rs
+
+src/main.rs:
---
+++ fstar/target/debug/deps/fstar-2523d4d24bc1e847.d
@@ -0,0 +1,5 @@
+/Users/domovoi/Desktop/Projects/scratchpad/fstar/target/debug/deps/libfstar-2523d4d24bc1e847.rmeta: src/main.rs
+
+/Users/domovoi/Desktop/Projects/scratchpad/fstar/target/debug/deps/fstar-2523d4d24bc1e847.d: src/main.rs
+
+src/main.rs:
