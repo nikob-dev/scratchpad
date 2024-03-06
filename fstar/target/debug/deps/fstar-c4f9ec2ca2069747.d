4c94899b3e8c364c2dc9427f26f03fc3231e05ad
Nikolas <nikolaslbussey@gmail.com>
2024-03-05 21:34:39 -0500

    start problem 1071

---
+++ fstar/target/debug/deps/fstar-c4f9ec2ca2069747.d
@@ -0,0 +1,5 @@
+/Users/domovoi/Desktop/Projects/scratchpad/fstar/target/debug/deps/fstar-c4f9ec2ca2069747: src/main.rs
+
+/Users/domovoi/Desktop/Projects/scratchpad/fstar/target/debug/deps/fstar-c4f9ec2ca2069747.d: src/main.rs
+
+src/main.rs:
