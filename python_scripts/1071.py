d4982d94d7ead5baf8382f65031342b140a5f2f5
Nikolas <nikolaslbussey@gmail.com>
2024-03-07 22:16:06 -0500

    1701 done for Python

--- python_scripts/1071.py
+++ python_scripts/1071.py
@@ -1,19 +1,47 @@
-class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str1) <= len(str2): #standardize which string will be the divisor
-            smallString = str1
-            bigString = str2
-        else:
-            smallString = str2
-            bigString = str1
-        #check if lengths of strings have GCD. this is needed to determine the size of the GCD string.
-        numerator = len(bigString)
-        denominator = len(smallString) #this is really a search function if you think about it. We're trying to find the smaller string in the larger string.
-        #Then step through larger string and compare it's ith value to the (i mod len(GCD))th value of the smaller string
-        
+
+def gcdOfStrings(str1: str, str2: str) -> str:
+    if(str1=="" or str2 ==""):
+        return ""
+
+    if len(str1) <= len(str2): #standardize which string will be the divisor
+        smallString = str1
+        bigString = str2
+    else:
+        smallString = str2
+        bigString = str1
+    #check if lengths of strings have GCD. this is needed to determine the size of the GCD string.
+    numerator = len(bigString)
+    denominator = len(smallString) #this is really a search function if you think about it. We're trying to find the smaller string in the larger string.
+    #Then step through larger string and compare it's ith value to the (i mod len(GCD))th value of the smaller string
+    smallNum = len(smallString)
+    bigNum = len(bigString)
+    GCDLength = 0
+    for ii in range(smallNum,0,-1):
+        if((smallNum%ii==0) & (bigNum%ii==0)):
+            GCDLength = ii
+            break   
+    
+    def checkString(entry: str, mod: int) -> bool:#check if string is a repeatable unit of size mod(i.e. the GCD size)
+        for ii in range (0, len(entry)):
+            if(entry[ii] != entry[ii%mod]):
+                return 0
+        return 1
+    
+    def compare2strings(string1: str, string2: str, mod: int) -> bool:#compare the first mod number of characters of each string
+        for ii in range (0, mod):
+            if(string1[ii] != string2[ii]):
+                return 0
+        return 1
+    if(checkString(smallString,GCDLength) & checkString(bigString,GCDLength) & compare2strings(smallString, bigString, GCDLength)):
+        return smallString[0:GCDLength]
+    else:
+        return ""
+
+print(gcdOfStrings("ABCABC","ABC"))
 
 
-print(4%2)
+
+
 #     '''For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
 
 # Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
